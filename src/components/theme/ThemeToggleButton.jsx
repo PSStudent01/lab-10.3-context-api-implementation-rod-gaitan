2f8efbf94fb33f9278950508537ce3e2c2c93a68
Rod Gaitan <rodgaitan01@gmail.com>
2026-03-01 23:33:54 -0500

    commit working on ThemeToggleButton Component

--- src/components/theme/ThemeToggleButton.jsx
+++ src/components/theme/ThemeToggleButton.jsx
@@ -1,15 +1,22 @@
 // ThemeToggleButton Component
 
-
+import { ThemeContext } from '../../contexts/ThemeContext'
+import { useContext } from 'react';
 
 function ThemeToggleButton() { 
 
+    const { theme, toggleTheme} = useContext(ThemeContext)
 
 
 
-
-
-
-
+return(
+    <div>
+        <button onclick={toggleTheme}>
+            {theme === 'Light' ? 'Dark Mode': 'Light Mode'}
+        </button>
+    </div>
+)
 
 }
+
+export default ThemeToggleButton
